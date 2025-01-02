a87d999366ebec18cc139f1b43728f614587159a
rtarun9 <rtarun0913@gmail.com>
2025-01-02 08:08:22 +0530

    Added cli options to choose platform / gpu api backend

--- premake5.lua
+++ premake5.lua
@@ -1,15 +1,26 @@
-local platform_backend = _ARGS["platform_backend"]
-local gpu_api_backend = _ARGS["gpu_api_backend"]
+local platform_backend = _OPTIONS["platform_backend"]
+local gpu_api_backend = _OPTIONS["gpu_api_backend"]
+
+if not platform_backend then
+    print("Error: platform_backend is required.")
+    os.exit(1)
+end
+
+if not gpu_api_backend then
+    print("Error: gpu_api_backend is required.")
+    os.exit(1)
+end
+
+print("Platform Backend: " .. platform_backend)
+print("GPU API Backend: " .. gpu_api_backend)
 
 project("ImGui")
 kind("StaticLib")
 language("C++")
 staticruntime("off")
+targetdir("bin/%{cfg.buildcfg}")
 
-targetdir("bin/" .. outputdir .. "/%{prj.name}")
-objdir("bin-int/" .. outputdir .. "/%{prj.name}")
-
-files({
+local files_to_add = {
     "imconfig.h",
     "imgui.h",
     "imgui.cpp",
@@ -21,17 +32,21 @@ files({
     "imstb_textedit.h",
     "imstb_truetype.h",
     "imgui_demo.cpp",
-})
+}
 
 if platform_backend == "win32" then
-    table.insert(files, "backend/imgui_impl_win32.h")
-    table.insert(files, "backend/imgui_impl_win32.cpp")
+    table.insert(files_to_add, "backends/imgui_impl_win32.h")
+    table.insert(files_to_add, "backends/imgui_impl_win32.cpp")
 end
 
 if gpu_api_backend == "dx12" then
-    table.insert(files, "backend/imgui_impl_dx12.h")
-    table.insert(files, "backend/imgui_impl_dx12.cpp")
+    table.insert(files_to_add, "backends/imgui_impl_dx12.h")
+    table.insert(files_to_add, "backends/imgui_impl_dx12.cpp")
 end
+
+includedirs('.')
+
+files(files_to_add)
 
 filter("system:windows")
 systemversion("latest")
